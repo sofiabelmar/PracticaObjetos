9a10370d7f685a168236b4a8f55401bb76d24a2e
Sofía Beltrán Martín <sofiabelmar@outlook.com>
2018-08-10 07:51:39 -0700

    Modificadores de Accesso

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,25 @@ namespace PrácticaObjetos
         static void Main(string[] args)
         {
             Persona persona1 = new Persona();
+            persona1.nombre = "Juan";
+            persona1.apellidos = "Perez";
+            persona1.edad = 20;
+            Console.WriteLine(persona1.nombre + " " +persona1.apellidos + " tiene " + persona1.edad + " años");
+
+            persona1.cumplirAños();
+
+            Console.WriteLine("Ahora " + persona1.nombre + " tiene " + persona1.edad + " años.");
+
+            persona1.altura = 1.5f;
+            //sharp toma los numeros deciales los toma de tipo double y ocupa más memoria, solo se le agrega la f al numero para cambiar el tipo de dato"
+
+            Console.WriteLine(persona1.nombre + " " + " mide " + persona1.altura + "m");
+
+            persona1.crecer(0.12f);
+
+            Console.WriteLine("Ahora " + persona1.nombre + " mide " + persona1.altura + "m");
+
+            Console.ReadLine();
         }
     }
 }
