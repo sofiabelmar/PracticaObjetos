c60e415c41b50b2991b1b0266ac4c9ba10313a35
Sofía Beltrán Martín <sofiabelmar@outlook.com>
2018-08-16 08:33:46 -0700

    CONSTRUCTORES

---
+++ ConsoleApp1/Contacto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrácticaObjetos
+{
+    class Contacto
+    {
+        public string Nombre { get; set; }
+        public string TelefonoCelular { get; set; }
+        public string TelefonodeCasa { get; set; }
+        public string Dirección { get; set; }
+        public string CorreoElectronico { get; set; }
+
+        public Contacto()
+        {
+            Nombre = "Nuevo Contacto";
+            TelefonodeCasa = "(000) 000 - 0000";
+
+        }
+
+        public Contacto(string nombre)
+        {
+            Nombre = nombre;
+
+        }
+        public Contacto(string telefonocasa, string nombre,  string telefonoCelular, string dirección, string correoElectronico)
+        {
+            Nombre = nombre;
+            TelefonodeCasa = telefonocasa;
+            TelefonoCelular = telefonoCelular;
+            Dirección = dirección;
+            CorreoElectronico = correoElectronico;
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace PrácticaObjetos
     {
         static void Main(string[] args)
         {
-            Persona persona1 = new Persona();
+            /*Persona persona1 = new Persona();
             persona1.nombre = "Juan";
             persona1.apellidos = "Perez";
             persona1.edad = 20;
@@ -29,7 +29,38 @@ namespace PrácticaObjetos
 
             Console.WriteLine("Ahora " + persona1.nombre + " mide " + persona1.altura + "m");
 
+            Console.ReadLine();*/
+
+            Contacto contaco1 =
+                new Contacto();
+
+            contaco1.Nombre = "Pedro Lopez";
+            contaco1.TelefonodeCasa = " (644) 131 605 ";
+            contaco1.Dirección = "Cananea #410";
+
+            Console.WriteLine("Contacto 1");
+            Console.WriteLine("Nombre: " + contaco1.Nombre);
+            Console.WriteLine("Telefono de Casa: " + contaco1.TelefonodeCasa);
+            Console.WriteLine("Dirección: " + contaco1.Dirección);
+
+            Contacto contacto2 =
+                new Contacto("Juan Perez");
+            Console.WriteLine("Contacto 2");
+            Console.WriteLine("Nomnbre: " + contacto2.Nombre);
+
+            Contacto contacto3 =
+                new Contacto("64410399330", "Maria Mendoza", "6441039930", "Tlaxcala #316", " mariammm@hotmail.com");
+            Console.WriteLine("Contacto 3");
+            Console.WriteLine("Nombre: " + contacto3.Nombre);
+            Console.WriteLine("Telefono de Casa: " + contacto3.TelefonodeCasa);
+            Console.WriteLine("Dirección: " + contacto3.Dirección);
+            Console.WriteLine("Telefono Celular: " + contacto3.TelefonoCelular);
+            Console.WriteLine("Correo Electronico: " + contacto3.TelefonoCelular);
+
+
             Console.ReadLine();
+
+
         }
     }
 }
